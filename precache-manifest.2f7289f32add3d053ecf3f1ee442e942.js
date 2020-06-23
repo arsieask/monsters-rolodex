1b610aa34977a5160a3030d1639f66e8cd08dae1
Arsen Askandaryan <arsieask@gmail.com>
2020-06-23 08:55:05 -0400

    Updates

---
+++ precache-manifest.2f7289f32add3d053ecf3f1ee442e942.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a953889a957e77699282a1a67e82065",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bef5f662fe468a8f7a85",
+    "url": "/monsters-rolodex/static/css/main.ab09491e.chunk.css"
+  },
+  {
+    "revision": "ce7c83e346e5fbe102d3",
+    "url": "/monsters-rolodex/static/js/2.aac49012.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.aac49012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef5f662fe468a8f7a85",
+    "url": "/monsters-rolodex/static/js/main.da0f19ec.chunk.js"
+  },
+  {
+    "revision": "6084ad0c13926bf12fd9",
+    "url": "/monsters-rolodex/static/js/runtime-main.b4aaacc4.js"
+  }
+]);
